3940408f1228811b9d0b4285a0a171e35b2ffaf8
choroalp <choroalp@skiff.com>
2025-01-12 19:47:57 +0300

    Major Update

--- main.py
+++ main.py
@@ -3,10 +3,12 @@
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
-from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import math
 
+camera_pos = [0.0, 0.0, 0.0]
+
+start_position = [0.0, 0.0, 0.0]
 
 def calculate_look_position(camera_pos, camera_rot):
     pitch_rad = math.radians(camera_rot[0])  # Convert pitch to radians
@@ -25,6 +27,7 @@ def calculate_look_position(camera_pos, camera_rot):
     )
     return look_pos
 
+
 vertices = [
     (1, -1, -1),
     (1, 1, -1),
@@ -35,7 +38,6 @@ vertices = [
     (-1, -1, 1),
     (-1, 1, 1),
 ]
-
 
 edges = [
     (0, 1),
@@ -66,14 +68,14 @@ def draw_multiple_cubes(cubes):
     for cube_vertices in cubes:
         draw_cube(cube_vertices)
 
-# Initialize Pygame and OpenGL
+
 pygame.init()
 running = True
 display = (1920, 1080)
 pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 gluPerspective(90, (display[0] / display[1]), 0.2, 50.0)
 
-camera_pos = [0.0, 0.0, 0.0]  # (x, y, z)
+player_pos = [0, 0, 0]
 camera_rot = [0.0, 0.0]
 
 # Set up the perspective projection
@@ -81,17 +83,16 @@ camera_rot = [0.0, 0.0]
 glTranslatef(0.0, 0.0, -5)
 glRotatef(0.0, 1, 0, 0)"""
 
-p_start_x = 4
-n_start_x = -4
+p_start_x = 0
+n_start_x = 0
 n_start_y = 0
 p_start_y = 0
 
 cubes = [
     vertices,
-    [(x + 2, y, z) for (x, y, z) in vertices],  # Cube 2 (translated)
-    [(x - 2, y, z) for (x, y, z) in vertices],  # Cube 3 (translated)
-    [(x + 4, y, z) for (x, y, z) in vertices]
 ]
+
+rotate = True
 
 clock = pygame.time.Clock()
 while running:
@@ -124,41 +125,41 @@ while running:
                 # Update all cubes by moving them along the Y-axis
                 cubes = [[(x - 0.1, y, z) for (x, y, z) in cube] for cube in cubes]
             elif event.key == pygame.K_RIGHT:
-                lp_start = p_start_x + 2
-                cubes.append([(x + lp_start, y, z) for (x, y, z) in vertices])
+                n_start_x += 2
+                cubes.append([(x + n_start_x, y, z) for (x, y, z) in vertices])
             elif event.key == pygame.K_LEFT:
+                p_start_x += 2
                 ln_start = 0 - p_start_x
-                cubes.append([(x + ln_start, y, z) for (x, y, z) in vertices])
+                cubes.append([(x + ln_start, y, z) for (x, y, z)in vertices])
             elif event.key == pygame.K_UP:
-                p_start_y += 1
+                p_start_y += 2
                 cubes.append([(x, y + p_start_y, z) for (x, y, z) in vertices])
             elif event.key == pygame.K_DOWN:
-                p_start_y -= 1
+                p_start_y -= 2
                 cubes.append([(x, y + p_start_y, z) for (x, y, z) in vertices])
             elif event.key == pygame.K_w:
+                player_pos[2] += 1
                 camera_pos[2] += 0.01
             elif event.key == pygame.K_s:
+                player_pos[2] -= 1
                 camera_pos[2] -= 0.01
             elif event.key == pygame.K_a:
+                player_pos[0] += 1
+                print(player_pos)
                 camera_pos[0] += 0.01
             elif event.key == pygame.K_d:
+                player_pos[2] -= 1
                 camera_pos[0] -= 0.01
-            elif event.key == pygame.K_o:
-                camera_rot[1] += 1
-            elif event.key == pygame.K_p:
-                camera_rot[1] -= 1
-            elif event.key == pygame.K_k:
-                camera_rot[0] += 1
-            elif event.key == pygame.K_l:
-                camera_rot[0] -= 1
-            elif event.key == pygame.K_q:
-                pygame.quit()
-                quit()
+            elif event.key == pygame.K_r:
+                glRotatef(1, 3, 1, 1)
+            elif event.key == pygame.K_g:
+                cubes.append([(player_pos[0], player_pos[1], player_pos[2]) for (x, y, z) in vertices])
 
     # Clear the screen and redraw the cubes with rotation
-    #glRotatef(1, 3, 1, 1)  # Rotate all cubes slightly on each frame
     glTranslatef(camera_pos[0], camera_pos[1], camera_pos[2])
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    print(player_pos)
 
     draw_multiple_cubes(cubes)  # Draw all cubes
     pygame.display.flip()
